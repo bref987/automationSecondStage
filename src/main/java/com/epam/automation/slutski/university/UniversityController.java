9343d71ec047b4560eeecd239c9175b193d0cb9e
Aleh Slutski <st.alehs@gmail.com>
2019-12-03 19:47:04 +0300

    refactor: log UniversityController output

--- src/main/java/com/epam/automation/slutski/university/UniversityController.java
+++ src/main/java/com/epam/automation/slutski/university/UniversityController.java
@@ -4,15 +4,18 @@ import com.epam.automation.slutski.university.exceptions.IncorrectNoteException;
 import com.epam.automation.slutski.university.exceptions.NoAnyCourseException;
 import com.epam.automation.slutski.university.exceptions.NoAnyUnityException;
 import com.epam.automation.slutski.university.logic.UniversityLogic;
+import com.epam.automation.slutski.university.structure.Faculty;
 import com.epam.automation.slutski.university.structure.Student;
 import com.epam.automation.slutski.university.students.StudentsList;
 import com.epam.automation.slutski.university.types.Courses;
 import com.epam.automation.slutski.university.types.Faculties;
-import com.epam.automation.slutski.university.view.Printer;
+import org.apache.log4j.Logger;
 
 import java.util.List;
 
 public class UniversityController {
+
+    static Logger logger = Logger.getLogger(UniversityController.class);
 
     public static void main(String[] args) throws NoAnyCourseException, IncorrectNoteException, NoAnyUnityException {
 
@@ -21,11 +24,11 @@ public class UniversityController {
 
             UniversityLogic university = new UniversityLogic(students);
 
-            Printer.print(university.getAverageUniversityCourseNote(Courses.CULTUROLOGY));
+            logger.info(university.getAverageUniversityCourseNote(Courses.CULTUROLOGY));
 
-            Printer.print(university.getAverageFacultyGroupCourseNote(Faculties.FRENCH, 110, Courses.LINGUISTICS));
+            logger.info(university.getAverageFacultyGroupCourseNote(Faculties.FRENCH, 110, Courses.LINGUISTICS));
 
-            Printer.print(students.get(0).getStudentAverageNote());
+            logger.info(students.get(0).getStudentAverageNote());
 
         } catch (NoAnyCourseException | IncorrectNoteException | NoAnyUnityException e) {
             e.printStackTrace();
--- src/main/java/com/epam/automation/slutski/university/view/Printer.java
+++
@@ -1,8 +0,0 @@
-package com.epam.automation.slutski.university.view;
-
-public class Printer {
-
-    public static void print(double d) {
-        System.out.println(d);
-    }
-}
